0711141a8fc11c4807001409fa0e7b0aaae5fdbd
Jakception <gbeulaygue@gmail.com>
2017-07-11 00:27:39 +0200

    Ajout Views Début

--- EBibliotheque/Controllers/HomeController.cs
+++ EBibliotheque/Controllers/HomeController.cs
@@ -24,7 +24,34 @@ namespace EBibliotheque.Controllers
         // GET: Home
         public ActionResult Index()
         {
+            List<Livre> listeLivres = dal.ObtientTousLesLivres();
+            return View(listeLivres);
+        }
+
+        public ActionResult AfficheAuteurs()
+        {
+            List<Auteur> listeAuteurs = dal.ObtientTousLesAuteurs();
+            return View(listeAuteurs);
+        }
+        public ActionResult AfficheLivre()
+        {
             return View();
+        }
+        [HttpPost]
+        public ActionResult AfficheLivre(int id)
+        {
+            Livre livre = dal.ChercheLivre(id);
+            return View(livre);
+        }
+        public ActionResult LivresPourUnAuteur()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult LivresPourUnAuteur(int id)
+        {
+            List<Livre> listeLivreAuteurs = dal.ObtientLivresDUnAuteur(id);
+            return View(listeLivreAuteurs);
         }
     }
 }
